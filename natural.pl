85ae62b6bbb3314357293340ad7f2d5219461177
Jón Gísli Egilsson <jongisli@Jons-MacBook-Pro.local>
2012-10-08 21:54:27 +0200

    allt i orden

--- natural.pl
+++ natural.pl
@@ -14,5 +14,25 @@ ismember(X,[X|_],X2) :- X2 = yes.
 ismember(X1,[_|XS],X3) :- ismember(X1,XS,X3).
 
 % Problem 4: the union/3 predicate
-%union([],X,X) :- true.
-%union([X1|X1s],[X2|X2s],[X3|X3s]) :-
+union([],X,X).
+union(X,[],X).
+union([X1|X1s],[X2|X2s],[X1|X3s]) :-
+	less(X1,X2),
+	union(X1s,[X2|X2s],X3s).
+union([X1|X1s],[X2|X2s],[X2|X3s]) :-
+	less(X2,X1),
+	union([X1|X1s],X2s,X3s).
+union([X|X1s],[X|X2s],[X|X3s]) :-
+	union(X1s,X2s,X3s).
+
+% Problem 5: the intersection/3 predicate
+intersection([],_,[]).
+intersection(_,[],[]).
+intersection([X|X1s],[X|X2s],[X|X3s]) :-
+	intersection(X1s,X2s,X3s).
+intersection([X1|X1s],[X2|X2s],X3) :-
+	less(X1,X2),
+	intersection(X1s,[X2|X2s],X3).
+intersection([X1|X1s],[X2|X2s],X3) :-
+	less(X2,X1),
+	intersection([X1|X1s],X2s,X3).
