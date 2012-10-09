585a88f8439e53b22e899ada597f6d44299ab71b
Jón Gísli Egilsson <jongisli@Jons-MacBook-Pro.local>
2012-10-09 10:10:43 +0200

    tests finishde

--- natural.pl
+++ natural.pl
@@ -36,3 +36,38 @@ intersection([X1|X1s],[X2|X2s],X3) :-
 intersection([X1|X1s],[X2|X2s],X3) :-
 	less(X2,X1),
 	intersection([X1|X1s],X2s,X3).
+
+
+:- begin_tests(natural).
+:- use_module(natural).
+% Testing 1.
+test(less_succeed) :- less(z, s(z)).
+test(less_fail, fail) :- less(s(z), z).
+test(less_derive, all(X == [z,s(z)])) :- less(X, s(s(z))).
+
+% Testing 2.
+test(checkset_succeed) :- checkset([s(z), s(s(s(z))), s(s(s(s(s(z)))))]).
+test(checkset_fail1, fail) :- checkset([s(s(s(z))), s(s(s(s(s(z))))), s(z)]).
+test(checkset_fail2, fail) :- checkset([z,z]).
+test(checkset_fail3, fail) :- checkset(z).
+test(checkset_derive, all(X == [z, s(z)])) :- checkset([X, s(s(z)), s(s(s(z)))]).
+
+% Testing 3.
+test(ismember_succeed) :- ismember(s(z), [z, s(z)], yes).
+test(ismember_fail, fail) :- ismember(s(s(z)), [z, s(z)], yes).
+%test(ismember_derive) :- ismember(N, [s(z), s(s(s(z)))], A).
+
+% Testing 4.
+test(union_succeed) :- union([z, s(s(z))], [s(z), s(s(z))], [z, s(z), s(s(z))]).
+test(union_fail, fail) :- union([z, s(s(z))], [z, s(s(z))], [z, s(z), s(s(z))]).
+test(union_derive, all(X = [[z], [z,s(z)], [z,s(z)]])) :- % same answer twice
+	union(X, [s(z)], [z,s(z)]). 
+
+% Testing 5.
+test(intersection_succeed) :-
+	intersection([z,s(s(z))], [s(s(z)), s(s(s(z)))], [s(s(z))]).
+test(intersection_fail, fail) :-
+	intersection([z,s(s(z))], [s(s(z)), s(s(s(z)))], [z, s(s(z))]).
+test(intersection_derive, all(A == [[]])) :- intersection([z], [s(z)], A).
+
+:- end_tests(natural).
